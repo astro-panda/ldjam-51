c67e56600ecd3993e13fa4543f3f977761fe0211
Jesseww <jessewhitesemail@gmail.com>
2022-10-01 23:28:05 -0500

    Added spawn points for stuff

--- FloppyFish/Scripts/Main.cs
+++ FloppyFish/Scripts/Main.cs
@@ -1,14 +1,24 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class Main : Node
 {
-    
+    [Export]
+    public PackedScene PlatformScene;
+    private Position2D _platformSpawnPos;
+
+    [Export]
+    public PackedScene Fish;
+    private Position2D _fishSpawnPos;
+
+    private List<CollectableType> _collectedCollectables;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _platformSpawnPos = GetNode<Position2D>("PlatformSpawnPosition");
+        _fishSpawnPos = GetNode<Position2D>("FishSpawnPosition");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,4 +26,9 @@ public class Main : Node
 //  {
 //      
 //  }
+
+    public void StartGame()
+    {
+
+    }
 }
