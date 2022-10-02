9f9c478e8153e5cc75eb96c081ce2d7af49ba267
Jesseww <jessewhitesemail@gmail.com>
2022-10-02 10:37:24 -0500

    Made progress on pull force

--- FloppyFish/Scripts/Platform.cs
+++ FloppyFish/Scripts/Platform.cs
@@ -2,15 +2,21 @@ using Godot;
 using System;
 
 public class Platform : RigidBody2D
-{
+{   
     public bool IsInWater = false;
+    [Export]
+    public float Damping = -1f;
+    [Export]
+    public float PullForce = 10f;
 
     public Vector2 WaterFlow = new Vector2((float)-0.946,(float)-0.326);
+
+    private Position2D _targetPos;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        _targetPos = GetNode<Position2D>("TargetPosition");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,6 +31,11 @@ public class Platform : RigidBody2D
         {
             ApplyCentralImpulse(20 * WaterFlow);
         }
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        AddCentralForce((_targetPos.Position - Position) * PullForce - (Vector2.Zero - LinearVelocity) * Damping);
     }
 
     public void OnWaterBodyEntered(PhysicsBody2D body)
