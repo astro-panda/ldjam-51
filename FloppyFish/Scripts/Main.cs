622e1113912bbdfefcf8b7522335d2427d2123a1
Jesseww <jessewhitesemail@gmail.com>
2022-10-02 00:44:57 -0500

    Finished platform spawner

--- FloppyFish/Scripts/Main.cs
+++ FloppyFish/Scripts/Main.cs
@@ -6,7 +6,10 @@ public class Main : Node
 {
     [Export]
     public PackedScene PlatformScene;
+    [Export]
+    public float PlatformSpawnInterval = 10;
     private Position2D _platformSpawnPos;
+    private Timer _platformSpawnTimer;
 
     [Export]
     public PackedScene Fish;
@@ -14,21 +17,26 @@ public class Main : Node
 
     private List<CollectableType> _collectedCollectables;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         _platformSpawnPos = GetNode<Position2D>("PlatformSpawnPosition");
+        _platformSpawnTimer = GetNode<Timer>("PlatformSpawnTimer");
         _fishSpawnPos = GetNode<Position2D>("FishSpawnPosition");
+
+        StartGame();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
-
+    // We can call this on button press later
     public void StartGame()
     {
+        _platformSpawnTimer.Start(2f);
+    }
 
+    public void OnPlatformSpawnTimeOut()
+    {
+        _platformSpawnTimer.Start(PlatformSpawnInterval);
+        Platform plat = (Platform)PlatformScene.Instance();
+        plat.Position = _platformSpawnPos.Position;
+        AddChild(plat);
     }
 }
